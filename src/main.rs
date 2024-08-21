8d14667aced6e898a511f23df6e214857fcff1c4
tnantoka <tnantoka@bornneet.com>
2024-08-21 22:22:35 +0900

    feat: basic file structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+mod rustman;
+
+use std::error;
+
+fn main() -> Result<(), Box<dyn error::Error>> {
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() < 2 {
+        return Err("Usage: rustman <command>".into());
+    }
+
+    match args[1].as_str() {
+        "new" => rustman::new::run(),
+        "build" => rustman::build::run(),
+        _ => return Err(format!("Unknown command: {}", args[1]).into()),
+    }
+
+    Ok(())
 }
